bce3c6387d69cc928d2940f8d23fdcace5549800
Max Vassilev <maximvassilev01@gmail.com>
2025-02-21 10:36:36 +0200

    Remove .env file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN } from "./constants";
 const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: "http://localhost:3000/",
 });
 
 api.interceptors.request.use(
--- frontend/src/pages/Request.jsx
+++ frontend/src/pages/Request.jsx
@@ -24,7 +24,7 @@ const Request = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}api/create-vehicle-request/`, {
+      const response = await fetch(`http://localhost:3000/api/create-vehicle-request/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
