ab569788692e99e09c8acbe77aba8e3e5ff2c15a
Max Vassilev <maximvassilev01@gmail.com>
2025-02-23 12:15:25 +0200

    Begin Azure DB configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,18 +89,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -144,3 +132,20 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
+
+
+# Database configuration:
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'v-t-database',
+#         'USER': 'dfcneoxiqq',
+#         'PASSWORD': 'LW6WQlmgN0vec$UP',
+#         'HOST': 'v-t-server.postgres.database.azure.com',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'sslmode': 'require',
+#         },
+#     }
+# }
