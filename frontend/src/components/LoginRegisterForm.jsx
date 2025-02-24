818189763ad0d037004b785fa7f65d92682f53a5
Max Vassilev <maximvassilev01@gmail.com>
2025-02-24 15:22:07 +0200

    Modify FE

--- frontend/src/components/LoginRegisterForm.jsx
+++ frontend/src/components/LoginRegisterForm.jsx
@@ -37,21 +37,21 @@ function Form({ route, method }) {
             <form onSubmit={handleSubmit} className="max-w-md w-full p-6 bg-gray-900 rounded-lg shadow-md">
                 <h1 className="text-2xl font-semibold text-center text-white mb-4">{name}</h1>
                 <input
-                    className="form-input block w-full p-2 border border-gray-700 rounded mb-4 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-600"
+                    className="form-input block w-full p-2 border border-gray-700 rounded mb-4 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     placeholder="Username"
                 />
                 <input
-                    className="form-input block w-full p-2 border border-gray-700 rounded mb-4 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-600"
+                    className="form-input block w-full p-2 border border-gray-700 rounded mb-4 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     placeholder="Password"
                 />
                 {loading && <LoadingIndicator />}
-                <button className="form-button w-full bg-purple-600 text-white py-2 rounded hover:bg-purple-700 transition duration-300" type="submit">
+                <button className="form-button w-full bg-gradient-to-r from-blue-600 to-indigo-700 text-white py-2 rounded hover:scale-105 transition duration-300 ease-in-out" type="submit">
                     {name}
                 </button>
                 {method === "login" && (
