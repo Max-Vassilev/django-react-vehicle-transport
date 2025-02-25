be3fa9fbe09f9cd4658466014fe13b8e9f212435
Max Vassilev <maximvassilev01@gmail.com>
2025-02-25 16:00:23 +0200

    .

--- backend/backend/api/models.py
+++ backend/backend/api/models.py
@@ -7,6 +7,8 @@ class VehicleTransportRequest(models.Model):
         ("pending_approval", "Pending Approval"),
         ("approved", "Approved"),
     ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=25)
