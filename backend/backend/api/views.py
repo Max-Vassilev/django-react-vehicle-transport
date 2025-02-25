d923be439b8c65ff286a482c0b52fbae2931a599
Max Vassilev <maximvassilev01@gmail.com>
2025-02-25 16:38:44 +0200

    Fix bug

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -17,10 +17,6 @@ class CreateVehicleTransportRequestView(generics.CreateAPIView):
     serializer_class = VehicleTransportRequestSerializer
     permission_classes = [AllowAny]
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-
 class AllVehicleTransportRequestsView(generics.ListAPIView):
     queryset = VehicleTransportRequest.objects.all()
     serializer_class = VehicleTransportRequestSerializer
