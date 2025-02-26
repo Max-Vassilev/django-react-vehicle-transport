9c8523c4d09cac1a21beab5606512cd5e16d6297
Max Vassilev <maximvassilev01@gmail.com>
2025-02-26 14:11:08 +0200

    004

--- backend/backend/api/models.py
+++ backend/backend/api/models.py
@@ -8,8 +8,6 @@ class VehicleTransportRequest(models.Model):
         ("approved", "Approved"),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=25)
     pickup_country = models.CharField(max_length=100)
     pickup_city = models.CharField(max_length=100)
@@ -28,4 +26,4 @@ class VehicleTransportRequest(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Request by {self.first_name} {self.last_name}"
+        return f"Request"
